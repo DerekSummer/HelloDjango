ec248428412fce9c1eae8093f09f6b01cc6d8ddc
dereksummer <xiech27@gmail.com>
2025-11-16 22:07:05 +0800

    Basic setup before start web service

--- mycmdb/views.py
+++ mycmdb/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello! You're at the index.")
+
+def mycmdb(request):
+    return HttpResponse("Hello! You're at the mycmdb.")
+
